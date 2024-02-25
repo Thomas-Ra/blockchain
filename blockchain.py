89f56d074ee6dd75564cab9fa8cb12912d6da9ec
Thomas-Ra <ThomasRaffel@t-online.de>
2024-02-25 12:14:12 +0100

    removed unicode string

--- blockchain.py
+++ blockchain.py
@@ -9,7 +9,7 @@ def hashMe(msg=""):
     if type(msg) != str:
         msg = json.dumps(msg, sort_keys = True)
     if sys.version_info.major == 2:
-        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
+        return (hashlib.sha256(msg).hexdigest(),'utf-8')
     else:
         return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
 
