dd39270aa8dd55e1087817317fdc5d9c5ebd82e0
Thomas-Ra <ThomasRaffel@t-online.de>
2024-02-25 11:58:35 +0100

    enable transactions on the blockchain and build the first blocks

---
+++ blockchain.py
@@ -0,0 +1,71 @@
+import hashlib
+import json
+import sys
+
+from transactions import makeTransaction, isValidTxn, updateState
+
+def hashMe(msg=""):
+    """helper function for wrapping hashing algo"""
+    if type(msg) != str:
+        msg = json.dumps(msg, sort_keys = True)
+    if sys.version_info.major == 2:
+        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
+    else:
+        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
+
+def makeBlock(txns, chain):
+    """each block collects a set of transactions, 
+    has a header and will be hashed and linked to the blockchain"""
+    parentBlock   = chain[-1]
+    parentHash    = parentBlock[u'hash']
+    blockNumber   = parentBlock[u'contents'] [u'blockNumber'] +1
+    txnCount      = len(txns)
+    blockContents = {u'blockNumber': blockNumber, u'parentHash': parentHash, u'txnCount': txnCount, 'txns': txns}
+    blockHash     = hashMe(blockContents)
+    block         = {u'hash': blockHash, u'contents': blockContents}
+
+    return block
+
+state = {u'Alice':50, u'Bob':50}
+
+# building genesis block that isnt linked to any other block (= starting block)
+genesisBlockTxns = [state]
+genesisBlockContents = {u'blockNumber':0,u'parentHash':None,u'txnCount':1,u'txns':genesisBlockTxns}
+genesisHash = hashMe(genesisBlockContents)
+genesisBlock = {u'hash':genesisHash,u'contents':genesisBlockContents}
+genesisBlockStr = json.dumps(genesisBlock, sort_keys=True)
+
+chain = [genesisBlock]
+
+blockSizeLimit = 5 # determines how many transactions one block will contain
+
+txnBuffer = [makeTransaction() for i in range(30)]
+
+while len(txnBuffer) > 0:
+    bufferStartSize = len(txnBuffer)
+    
+    ## Gather a set of valid transactions for inclusion
+    txnList = []
+    while (len(txnBuffer) > 0) & (len(txnList) < blockSizeLimit):
+        newTxn = txnBuffer.pop()
+        validTxn = isValidTxn(newTxn,state) # This will return False if txn is invalid
+        
+        if validTxn:           # If we got a valid state, not 'False'
+            txnList.append(newTxn)
+            state = updateState(newTxn,state)
+        else:
+            print("ignored transaction")
+            sys.stdout.flush()
+            continue  # This was an invalid transaction; ignore it and move on
+        
+## Make a block
+myBlock = makeBlock(txnList,chain)
+chain.append(myBlock)
+
+chain[0]
+chain[1]
+
+print("\n", chain[0], "\n")
+print("\n", chain[1], "\n")
+print("\n", state, "\n")
+
---
+++ transactions.py
@@ -0,0 +1,76 @@
+import random
+random.seed(0)
+
+def makeTransaction(maxValue = 3):
+    """ method to generate exchanges between two (and only two) personas
+    depostits will be positive, withdrawls negative
+    no money can be created or destroyed
+    """
+
+    #choose either -1 or 1
+    sign = int(random.getrandbits(1))*2 - 1
+    amount = random.randint(1, maxValue)
+    alicePays = sign * amount
+    bobPays = -1 * alicePays
+
+    return {u'Alice': alicePays, u'Bob': bobPays}
+
+def updateState (txn, state):
+    """ method to validate the transactions
+    a user can only make a withdrawl with enough funds to cover it
+    """
+
+    state = state.copy()
+
+    for key in txn:
+        if key in state.keys():
+            state[key] += txn[key]
+        else:
+            state[key] = txn[key]
+    return state
+
+def isValidTxn(txn, state):
+    """second method to validate the transactions
+    the sum of all withdrawls and deposits have to be zero (no money is generated out of thin air)
+    """
+    if sum(txn.values()) != 0:
+        return False
+    
+    #check if a user tries to spend money he dosent has
+    for key in txn.keys():
+        if key in state.keys():
+            acctBalance =  state[key]
+        else:
+            acctBalance = 0
+        if(acctBalance +  txn[key]) < 0:
+            return False
+        
+    return True
+
+#create a set of transactions
+txnBuffer = [makeTransaction() for i in range(30)]
+
+#do various transactions with a fix amount of money
+state = {u'Alice':5, u'Bob':5}
+"""
+print("\nAlice and Bob start both with 5 money! First transaction Alice gives 3 money to bob.")
+print(isValidTxn({u'Alice': -3, u'Bob': 3},state))
+print("Transaction is valid. Alice has enough money to cover it.")
+
+print("\nAlice wants to give an unequal amount of money to bob.")
+print(isValidTxn({u'Alice': -4, u'Bob': 3},state))  # But we can't create or destroy tokens!
+print("Transaction fails. Unequal money transfer are not eligble.")
+
+print("\nAlice wants to give 6 money to bob.")
+print(isValidTxn({u'Alice': -6, u'Bob': 6},state))  # We also can't overdraft our account.
+print("Transaction fails. Alice has not enough funds.")
+
+print("\nA new user is created. The amount of money from all three equals the start amount (10).")
+print(isValidTxn({u'Alice': -4, u'Bob': 2,'Lisa':2},state)) # Creating new users is valid
+print("New users can be created. Only 10 money is in the system.")
+
+print("\nBob magically gets an additional money to his funds.")
+print(isValidTxn({u'Alice': -4, u'Bob': 3,'Lisa':2},state)) # But the same rules still apply!
+print("Fails. Sum of all money would be 11 and not 10.")
+"""
+
