6daa03935770a0c021d3a77915b0f48e34354b88
Thomas-Ra <ThomasRaffel@t-online.de>
2024-02-26 13:54:30 +0100

    blockchain processes and adds new transactions

--- classes/blockchain.py
+++ classes/blockchain.py
@@ -1,6 +1,14 @@
-import hashlib
-import json
-import sys
+import hashlib, json, sys
+
+from classes.transactions import isValidTxn, updateState, makeTransaction
+
+def printChain(state, chain):
+    """helper function to print the blockchain"""
+    for i in range(len(chain)):
+        print("blockNumber: ", i)
+        print(chain[i])
+        print("\n")
+    print(state)
 
 def hashMe(msg=""):
     """helper function for wrapping hashing algo"""
@@ -23,3 +31,47 @@ def makeBlock(txns, chain):
     block         = {u'hash': blockHash, u'contents': blockContents}
 
     return block
+
+def generateGenesisBlock(state):
+    """ method to generate the genesis block of the blockchain
+    the genesis block is the first block of the blockchain and is not linked to any other block
+    """ 
+    genesisBlockTxns = [state]
+    genesisBlockContents = {u'blockNumber':0, u'parentHash':None, u'txnCount':1, u'txns':genesisBlockTxns}
+    genesisHash = hashMe(genesisBlockContents)
+    genesisBlock = {u'hash':genesisHash, u'contents':genesisBlockContents}
+    genesisBlockStr = json.dumps(genesisBlock, sort_keys = True)
+    return genesisBlock
+
+def processTransactions(txnBuffer, blockSizeLimit, state):
+    """ method to gather a set of valid transactions for inclusion in the next block
+    """
+    txnList = []
+    while (len(txnBuffer) > 0) & (len(txnList) < blockSizeLimit):
+        newTxn = txnBuffer.pop()
+        validTxn = isValidTxn(newTxn, state)
+                    
+        if validTxn:
+            txnList.append(newTxn)
+            state = updateState(newTxn, state)
+        else:
+            print("ignored transaction")
+            sys.stdout.flush()
+            continue
+                
+    return txnList, state
+
+
+def makeBlockWithRandomTxns(state, chain, blockSizeLimit):
+    """ method to create a block with a set of transactions
+    create a set of transactions
+    should be the same as the blocksize limit so that the transactions fit into one block
+    otherwise you will need to create more blocks
+    """
+    txnBuffer = [makeTransaction() for i in range(blockSizeLimit)] 
+    txnList, state = processTransactions(txnBuffer, blockSizeLimit, state)
+
+    myBlock = makeBlock(txnList, chain) # make a block
+    chain.append(myBlock) # make the block part of the blockchain
+    # printChain(state, chain)
+    return state
