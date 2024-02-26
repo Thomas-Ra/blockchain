aa1b5ebf930069f1dd1facae3d927bf7a9493e4a
Thomas-Ra <ThomasRaffel@t-online.de>
2024-02-26 11:51:50 +0100

    test transactions in transactions.py rather than demoChain.py

--- classes/transactions.py
+++ classes/transactions.py
@@ -47,3 +47,25 @@ def isValidTxn(txn, state):
             return False
         
     return True
+
+#testing the method to validate transactions
+state = {u'Alice':5, u'Bob':5}
+print("\nAlice and Bob start both with 5 money! First transaction Alice gives 3 money to bob.")
+print(isValidTxn({u'Alice': -3, u'Bob': 3},state))
+print("Transaction is valid. Alice has enough money to cover it.")
+
+print("\nAlice wants to give an unequal amount of money to bob.")
+print(isValidTxn({u'Alice': -4, u'Bob': 3},state))  # But we can't create or destroy tokens!
+print("Transaction fails. Unequal money transfer are not eligble.")
+
+print("\nAlice wants to give 6 money to bob.")
+print(isValidTxn({u'Alice': -6, u'Bob': 6},state))  # We also can't overdraft our account.
+print("Transaction fails. Alice has not enough funds.")
+
+print("\nA new user is created. The amount of money from all three equals the start amount (10).")
+print(isValidTxn({u'Alice': -4, u'Bob': 2,'Lisa':2},state)) # Creating new users is valid
+print("New users can be created. Only 10 money is in the system.")
+
+print("\nBob magically gets an additional money to his funds.")
+print(isValidTxn({u'Alice': -4, u'Bob': 3,'Lisa':2},state)) # But the same rules still apply!
+print("Fails. Sum of all money would be 11 and not 10.")
